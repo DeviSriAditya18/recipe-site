4a1959040b7bc557088fea0cd40f3af92006013d
Aditya <devisriaditya17@gmail.com>
2025-02-02 00:59:47 +0530

    undo all changes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const connectDB = require('./config/db');
 const recipeRoutes = require('./routes/recipeRoutes');
 const cors = require('cors');
-const path=require('path');
 require('dotenv').config();
 
 const app = express();
@@ -12,20 +11,6 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-
-const _dirname=path.resolve();
-if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname,'/frontend/build')));
-
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"frontend","build","index.html"));
-  })
-}
-else{
-  app.get("/",(req,res)=>{
-    res.send("API is running");
-  });
-}
 
 // Connect to MongoDB
 connectDB();
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,20 +1,10 @@
-const path = require('path');
-
 module.exports = {
-  webpack: function (config, env) {
-    // Adding polyfill for Node.js core modules like 'stream'
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      stream: require.resolve('stream-browserify')
-    };
+    resolve: {
+      fallback: {
+        stream: require.resolve("stream-browserify")
+      }
+    }
+  };
 
-    // Customizing the output directory and filename
-    config.output = {
-      ...config.output,
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'index.js',  // You can change this to whatever filename you need
-    };
-
-    return config;
-  }
-};
+  
+  
